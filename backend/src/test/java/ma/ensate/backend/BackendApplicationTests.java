15da9e469fa84c02a0c4f714291b315283982ae5
elbejauinada <151253495+elbejauinada@users.noreply.github.com>
2026-01-02 01:11:07 +0100

    springboot add

---
+++ backend/src/test/java/ma/ensate/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ensate.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
