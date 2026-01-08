03dc1a489c28ca3b1038eef700bd4ae4f0fdbb63
Skidis17 <moha-sadki@outlook.com>
2026-01-08 02:41:14 +0100

    Dtos

---
+++ backend/src/main/java/ma/ensate/backend/dto/DemandeDto.java
@@ -0,0 +1,22 @@
+package ma.ensate.backend.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DemandeDto {
+    private Long id;
+    private String type;
+    private LocalDate dateDebut;
+    private LocalDate dateFin;
+    private String motif;
+    private String justificatifUrl;
+    private String statut;
+    private Long createdBy;
+    private LocalDateTime createdAt;
+}
